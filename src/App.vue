a7681f580e434834be982f11231e7b3d10de3343
Afif Alfiano <afif.alfiano@uii.ac.id>
2022-03-18 19:53:10 +0700

    feat: add menu and content dummy

--- src/App.vue
+++ src/App.vue
@@ -13,10 +13,37 @@
     </div>
     </v-app-bar>
 
-    <v-main class="d-xs mx-auto">
+    <v-main class="d-xs mx-auto overflow-y-auto px-4" style="max-width:600px">
       <!-- <HelloWorld/> -->
       <h1 class="mt-5">Hello World</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem illum veritatis provident. Consequuntur eius soluta, quaerat cupiditate delectus expedita, maxime id tempore, eum voluptatibus vel! Similique obcaecati possimus error consequatur at assumenda aut libero, excepturi veniam explicabo hic velit quae illum alias necessitatibus dignissimos! Velit, quis dignissimos hic repellat totam corrupti eum ex vel rem, quo aspernatur. Laudantium commodi ullam quod eveniet similique expedita temporibus mollitia quidem, illo quis possimus doloribus. Vel, fugiat minima natus nulla modi doloremque quos consequatur molestiae at exercitationem saepe hic eveniet debitis animi nihil quibusdam cumque sequi cum excepturi veniam aspernatur quidem? Aspernatur, tenetur consequuntur.</p>
+
+      <h2 class="mt-5">Hello Guys</h2>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem illum veritatis provident. Consequuntur eius soluta, quaerat cupiditate delectus expedita, maxime id tempore, eum voluptatibus vel! Similique obcaecati possimus error consequatur at assumenda aut libero, excepturi veniam explicabo hic velit quae illum alias necessitatibus dignissimos! Velit, quis dignissimos hic repellat totam corrupti eum ex vel rem, quo aspernatur. Laudantium commodi ullam quod eveniet similique expedita temporibus mollitia quidem, illo quis possimus doloribus. Vel, fugiat minima natus nulla modi doloremque quos consequatur molestiae at exercitationem saepe hic eveniet debitis animi nihil quibusdam cumque sequi cum excepturi veniam aspernatur quidem? Aspernatur, tenetur consequuntur.</p>
+
+      <h2 class="mt-5">Hello Guys</h2>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem illum veritatis provident. Consequuntur eius soluta, quaerat cupiditate delectus expedita, maxime id tempore, eum voluptatibus vel! Similique obcaecati possimus error consequatur at assumenda aut libero, excepturi veniam explicabo hic velit quae illum alias necessitatibus dignissimos! Velit, quis dignissimos hic repellat totam corrupti eum ex vel rem, quo aspernatur. Laudantium commodi ullam quod eveniet similique expedita temporibus mollitia quidem, illo quis possimus doloribus. Vel, fugiat minima natus nulla modi doloremque quos consequatur molestiae at exercitationem saepe hic eveniet debitis animi nihil quibusdam cumque sequi cum excepturi veniam aspernatur quidem? Aspernatur, tenetur consequuntur.</p>
     </v-main>
+
+      <v-bottom-navigation
+      style="max-width: 600px; baground-color: none"
+      class="mx-auto d-flex justify-space-around"
+    :value="value"
+    color="teal"
+    grow
+  >
+    <v-btn class="fill-height my-3">
+      <v-icon>mdi-home</v-icon>
+    </v-btn>
+
+    <v-btn class="fill-height my-3">
+      <v-icon>mdi-account</v-icon>
+    </v-btn>
+
+    <v-btn class="fill-height my-3">
+      <v-icon>mdi-logout</v-icon>
+    </v-btn>
+  </v-bottom-navigation>
   </v-app>
 </template>
 
@@ -31,7 +58,8 @@ export default {
   },
 
   data: () => ({
-    //
+    value: 0,
+    active: true,
   }),
 };
 </script>
