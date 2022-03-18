1e6b8e5273920e885cf360dfcc4e247aefb966d3
Afif Alfiano <afif.alfiano@uii.ac.id>
2022-03-18 19:58:45 +0700

    fix: wrapper content

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
     <v-app-bar
       app
       class="mx-auto"
-      color="primary"
+      color="teal"
       max-width="600"
       justify="center"
       dark
@@ -13,9 +13,12 @@
     </div>
     </v-app-bar>
 
-    <v-main class="d-xs mx-auto overflow-y-auto px-4" style="max-width:600px">
+    <v-main class="d-xs mx-auto overflow-y-auto elevation-4" style="max-width:600px">
       <!-- <HelloWorld/> -->
-      <h1 class="mt-5">Hello World</h1>
+      <v-container>
+        <v-row>
+          <v-col cols="12">
+      <h1>Hello World</h1>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem illum veritatis provident. Consequuntur eius soluta, quaerat cupiditate delectus expedita, maxime id tempore, eum voluptatibus vel! Similique obcaecati possimus error consequatur at assumenda aut libero, excepturi veniam explicabo hic velit quae illum alias necessitatibus dignissimos! Velit, quis dignissimos hic repellat totam corrupti eum ex vel rem, quo aspernatur. Laudantium commodi ullam quod eveniet similique expedita temporibus mollitia quidem, illo quis possimus doloribus. Vel, fugiat minima natus nulla modi doloremque quos consequatur molestiae at exercitationem saepe hic eveniet debitis animi nihil quibusdam cumque sequi cum excepturi veniam aspernatur quidem? Aspernatur, tenetur consequuntur.</p>
 
       <h2 class="mt-5">Hello Guys</h2>
@@ -23,10 +26,13 @@
 
       <h2 class="mt-5">Hello Guys</h2>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem illum veritatis provident. Consequuntur eius soluta, quaerat cupiditate delectus expedita, maxime id tempore, eum voluptatibus vel! Similique obcaecati possimus error consequatur at assumenda aut libero, excepturi veniam explicabo hic velit quae illum alias necessitatibus dignissimos! Velit, quis dignissimos hic repellat totam corrupti eum ex vel rem, quo aspernatur. Laudantium commodi ullam quod eveniet similique expedita temporibus mollitia quidem, illo quis possimus doloribus. Vel, fugiat minima natus nulla modi doloremque quos consequatur molestiae at exercitationem saepe hic eveniet debitis animi nihil quibusdam cumque sequi cum excepturi veniam aspernatur quidem? Aspernatur, tenetur consequuntur.</p>
+      </v-col>
+      </v-row>
+    </v-container>
     </v-main>
 
       <v-bottom-navigation
-      style="max-width: 600px; baground-color: none"
+      style="max-width: 600px; background-color: none"
       class="mx-auto d-flex justify-space-around"
     :value="value"
     color="teal"
