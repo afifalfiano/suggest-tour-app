9f3d7b0ee896695e87247dde485d348945feb312
Afif Alfiano <afif.alfiano@uii.ac.id>
2022-03-18 19:14:25 +0700

    feat: change screen to first mobile

--- src/App.vue
+++ src/App.vue
@@ -1,56 +1,33 @@
 <template>
-  <v-app>
+  <v-app class="d-xs" >
     <v-app-bar
       app
+      class="mx-auto"
       color="primary"
+      max-width="600"
+      justify="center"
       dark
     >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
+    <div class="d-flex align-center justify-center">
+      <h1 class="display-1 font-weight-bold">Suggest Tour App</h1>
+    </div>
     </v-app-bar>
 
-    <v-main>
-      <HelloWorld/>
+    <v-main class="d-xs mx-auto">
+      <!-- <HelloWorld/> -->
+      <h1 class="mt-5">Hello World</h1>
     </v-main>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
+// import HelloWorld from './components/HelloWorld';
 
 export default {
   name: 'App',
 
   components: {
-    HelloWorld,
+    // HelloWorld,
   },
 
   data: () => ({
