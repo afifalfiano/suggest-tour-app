df48a567a5d84300adb017916b1a7f6b78efa953
Afif Alfiano <afif.alfiano@uii.ac.id>
2022-03-20 19:02:20 +0700

    fix: fix pwa

--- vue.config.js
+++ vue.config.js
@@ -1,17 +1,17 @@
 // const { defineConfig } = require('@vue/cli-service')
 module.exports = {
-  publicPath: '',
-  pwa: {
-    themeColor: "#42b983",
-    msTileColor: "#42b983",
-    appleMobileWebAppCache: "yes",
-    manifestOptions: {
-      background_color: "#42b983"
-    },
-    workboxPluginMode: "InjectManifest",
-        workboxOptions:{
-            swSrc:"./src/service-worker.js",
-            swDest: "service-worker.js"
-        }
-  }
+  // publicPath: '',
+  // pwa: {
+  //   themeColor: "#42b983",
+  //   msTileColor: "#42b983",
+  //   appleMobileWebAppCache: "yes",
+  //   manifestOptions: {
+  //     background_color: "#42b983"
+  //   },
+  //   workboxPluginMode: "InjectManifest",
+  //       workboxOptions:{
+  //           swSrc:"./src/service-worker.js",
+  //           swDest: "service-worker.js"
+  //       }
+  // }
 }
