cfcfbb4eee92e9415dca3d62211a1efff5541945
Afif Alfiano <afif.alfiano@uii.ac.id>
2022-03-20 20:03:31 +0700

    fix: pwa

--- vue.config.js
+++ vue.config.js
@@ -13,7 +13,8 @@ module.exports = defineConfig({
     },
     workboxPluginMode: 'InjectManifest',
     workboxOptions: {
-      swSrc: './src/service-worker.js'
+      swSrc: './src/service-worker.js',
+      swDest: 'service-worker.js',
     },
     name: 'Suggest Tour App'
   }
