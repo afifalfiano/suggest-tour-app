14f8fdf015b65aaa8e2c050e46cc4e21fd3a2602
Afif Alfiano <afif.alfiano@uii.ac.id>
2022-03-20 18:16:49 +0700

    fix: pwa

--- vue.config.js
+++ vue.config.js
@@ -2,5 +2,14 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: [
     'vuetify'
-  ]
+  ],
+  publicPath: './',
+  pwa: {
+    themeColor: "#42b983",
+    msTileColor: "#42b983",
+    appleMobileWebAppCache: "yes",
+    manifestOptions: {
+      background_color: "#42b983"
+    }
+  }
 })
