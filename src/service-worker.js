9728722c482808725042c8bdcc2426ac700a5dc1
Afif Alfiano <afif.alfiano@uii.ac.id>
2022-03-20 20:22:33 +0700

    fix: pwa

--- src/service-worker.js
+++ src/service-worker.js
@@ -72,8 +72,8 @@ workbox.strategies.staleWhileRevalidate({
   })
 );
 
-// self.addEventListener('message', (event) => {
-//   if (event.data && event.data.type === 'SKIP_WAITING') {
-//     self.skipWaiting();
-//   }
-// });
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
